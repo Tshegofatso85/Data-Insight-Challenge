37dfbab3fc3cc2f384743b87a71edfc9893c0550
Tshegofatso85 <generalmosha85@gmail.com>
2026-04-28 02:27:37 +0200

    updated style

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -1,6 +1,6 @@
 export default function Summary({ summary }) {
   return (
-    <div style={{ marginBottom: "20px" }}>
+    <div style={{ margin: "20px" }}>
       <h2>Summary</h2>
       <p style={{ lineHeight: "1.6", color: "#cbd5e1" }}>{summary}</p>
     </div>
